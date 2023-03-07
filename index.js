50f3b69f46f79c1f9b4caf87086a5823e3ef8082
ali <aifeqwe@yahoo.com>
2023-03-08 01:06:18 +0330

    debug

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import ImagePicker from 'react-native-image-picker';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('FrameExtractor', () => App);
+
+ImagePicker.launchImageLibrary({}, (response) => {
+  console.log('Response = ', response);
+ 
+  if (response.didCancel) {
+    console.log('User cancelled image picker');
+  } else if (response.error) {
+    console.log('ImagePicker Error: ', response.error);
+  } else {
+    const source = { uri: response.uri };
+ 
+    // You can also display the image using data:
+    // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+ 
+    this.setState({
+      imageSource: source,
+    });
+  }
+});
